c685ca41f657f94b977339a7d5e2e4e13a74861b
pradika pratama <33866097+pradprat@users.noreply.github.com>
2019-09-12 16:56:41 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,105 +1,148 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css">
-    </head>
-    <body>
-        <canvas id="myChart" width="100" height="50"></canvas>
-        <div id="chartContainer" style="height: 300px; width: 100%;"></div>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-      <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-      <script>
-      var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	exportEnabled: true,
-	theme: "light1", // "light1", "light2", "dark1", "dark2"
-	title:{
-		text: "Simple Column Chart with Index Labels"
-	},
-	data: [{
-		type: "column", //change type to bar, line, area, pie, etc
-		//indexLabel: "{y}", //Shows y value on all Data Points
-		indexLabelFontColor: "#5A5757",
-		indexLabelPlacement: "outside",
-		dataPoints: [
-			{ x: 10, y: 71 },
-			{ x: 20, y: 55 },
-			{ x: 30, y: 50 },
-			{ x: 40, y: 65 },
-			{ x: 50, y: 92, indexLabel: "Highest" },
-			{ x: 60, y: 68 },
-			{ x: 70, y: 38 },
-			{ x: 80, y: 71 },
-			{ x: 90, y: 54 },
-			{ x: 100, y: 60 },
-			{ x: 110, y: 36 },
-			{ x: 120, y: 49 },
-			{ x: 130, y: 21, indexLabel: "Lowest" }
-		]
-	}]
-});
-chart.render();
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css">
+</head>
+
+<body>
+    <canvas id="myChart" width="100" height="50"></canvas>
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script>
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            exportEnabled: true,
+            theme: "light1", // "light1", "light2", "dark1", "dark2"
+            title: {
+                text: "Simple Column Chart with Index Labels"
+            },
+            data: [{
+                type: "column", //change type to bar, line, area, pie, etc
+                //indexLabel: "{y}", //Shows y value on all Data Points
+                indexLabelFontColor: "#5A5757",
+                indexLabelPlacement: "outside",
+                dataPoints: [{
+                        x: 10,
+                        y: 71
+                    },
+                    {
+                        x: 20,
+                        y: 55
+                    },
+                    {
+                        x: 30,
+                        y: 50
+                    },
+                    {
+                        x: 40,
+                        y: 65
+                    },
+                    {
+                        x: 50,
+                        y: 92,
+                        indexLabel: "Highest"
+                    },
+                    {
+                        x: 60,
+                        y: 68
+                    },
+                    {
+                        x: 70,
+                        y: 38
+                    },
+                    {
+                        x: 80,
+                        y: 71
+                    },
+                    {
+                        x: 90,
+                        y: 54
+                    },
+                    {
+                        x: 100,
+                        y: 60
+                    },
+                    {
+                        x: 110,
+                        y: 36
+                    },
+                    {
+                        x: 120,
+                        y: 49
+                    },
+                    {
+                        x: 130,
+                        y: 21,
+                        indexLabel: "Lowest"
+                    }
+                ]
+            }]
+        });
+        chart.render();
 
         function addData(chart, label, data) {
             chart.data.labels.push(label);
@@ -113,10 +156,10 @@ chart.render();
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels:[],
+                labels: [],
                 datasets: [{
                     label: '',
-                    data:[],
+                    data: [],
                     backgroundColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
@@ -158,18 +201,18 @@ chart.render();
                 }
             }
         });
-        
+
         fetch('http://localhost:8000/api/opensignal/v_opensignal_national_monthly/tech/4G/date/201906')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(myJson) {
-            myJson.forEach(element => {
-                addData(myChart, element.Network_id,element.Download_kbps );
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(myJson) {
+                myJson.forEach(element => {
+                    addData(myChart, element.Network_id, element.Download_kbps);
+                });
+
             });
-            
-        });
-        
-        </script>
-    </body>
+    </script>
+</body>
+
 </html>
